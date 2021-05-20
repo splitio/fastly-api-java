6c12933002ef69f9a6dbe887a61d18bb3d5b4b04
Taina Caetano <taina.caetano@split.io>
2021-05-20 13:43:50 -0700

    NOC-518: [1.2.2] Overload methods to allow custom fastly urls
    This commit overloads all methods in FastlyApiClient to allow a custom fastly url to be used by the callers. By default, all existing methods that do not have that parameter will use the existing FASTLY_URL constant and have no change in behavior.
    
    This is in order to support split-incident-31 investigations and allow WebAdmin to, on-demand for specific organizations, use the Fastly Proxy service to purge instead of going directly to api.fastly.com

--- src/test/java/io/split/fastly/client/FastlyApiClientTest.java
+++ src/test/java/io/split/fastly/client/FastlyApiClientTest.java
@@ -24,6 +24,7 @@ public class FastlyApiClientTest {
     public static final String API_KEY = "someApikey";
     public static final String SERVICE_ID = "someServiceId";
     public static final List<String> SURROGATE_KEYS = Arrays.asList("key1", "key2");
+    public static final String CUSTOM_FASTLY_URL = "http://custom_fastly_url.com";
 
     @Mock
     private FastlyApiClient.AsyncHttpExecutor executor;
@@ -74,6 +75,42 @@ public class FastlyApiClientTest {
                 parametersCaptor.capture());
 
         assertThat(urlCaptor.getValue(), is(String.format("%s/service/%s/purge", FastlyApiClient.FASTLY_URL, SERVICE_ID)));
+        assertThat(methodCaptor.getValue(), is(FastlyApiClient.Method.POST));
+
+        Map<String, String> headers = headersCaptor.getValue();
+        assertThat(headers.get("Fastly-Key"), is(API_KEY));
+        assertThat(headers.get("Accept"), is("application/json"));
+        assertThat(headers.get("User-Agent"), is("fastly-api-java-v" + VersionResolver.instance().getVersion()));
+        assertThat(headers.get("Surrogate-Key"), is(FastlyApiClient.SURROGATE_KEY_JOINER.join(SURROGATE_KEYS)));
+        assertThat(headers.get("Fastly-Soft-Purge"), is("1"));
+    }
+
+    @Test
+    public void testPurgeKeysWithCustomFastlyUrl() {
+        fastlyApiClient.purgeKeys(SURROGATE_KEYS, CUSTOM_FASTLY_URL);
+
+        Mockito.verify(executor).execute(urlCaptor.capture(), methodCaptor.capture(), headersCaptor.capture(),
+                parametersCaptor.capture());
+
+        assertThat(urlCaptor.getValue(), is(String.format("%s/service/%s/purge", CUSTOM_FASTLY_URL, SERVICE_ID)));
+        assertThat(methodCaptor.getValue(), is(FastlyApiClient.Method.POST));
+
+        Map<String, String> headers = headersCaptor.getValue();
+        assertThat(headers.get("Fastly-Key"), is(API_KEY));
+        assertThat(headers.get("Accept"), is("application/json"));
+        assertThat(headers.get("User-Agent"), is("fastly-api-java-v" + VersionResolver.instance().getVersion()));
+        assertThat(headers.get("Surrogate-Key"), is(FastlyApiClient.SURROGATE_KEY_JOINER.join(SURROGATE_KEYS)));
+        assertThat(headers.get("Fastly-Soft-Purge"), is(nullValue()));
+    }
+
+    @Test
+    public void testSoftPurgeKeysWithCustomFastlyUrl() {
+        fastlyApiClient.softPurgeKeys(SURROGATE_KEYS, CUSTOM_FASTLY_URL);
+
+        Mockito.verify(executor).execute(urlCaptor.capture(), methodCaptor.capture(), headersCaptor.capture(),
+                parametersCaptor.capture());
+
+        assertThat(urlCaptor.getValue(), is(String.format("%s/service/%s/purge", CUSTOM_FASTLY_URL, SERVICE_ID)));
         assertThat(methodCaptor.getValue(), is(FastlyApiClient.Method.POST));
 
         Map<String, String> headers = headersCaptor.getValue();
